f00674874ebb498f028d151597e96b22d5e385ba
hamitmizrak <hamitmizrak@gmail.com>
2022-11-15 11:29:10 +0300

    Data config

--- src/main/java/com/hamitmizrak/BilgincJavaScriptApplication.java
+++ src/main/java/com/hamitmizrak/BilgincJavaScriptApplication.java
@@ -3,11 +3,19 @@ package com.hamitmizrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+//exclude:dahil etmemek
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        // org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+//@SpringBootApplication
 public class BilgincJavaScriptApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BilgincJavaScriptApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BilgincJavaScriptApplication.class, args);
+    }
 
 }
