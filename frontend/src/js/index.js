5938df72a867269506845fe89b046708648f84d7
hamitmizrak <hamitmizrak@gmail.com>
2022-11-16 10:09:19 +0300

    Login Data

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -27,11 +27,40 @@ $(document).ready(function () {
 
 // login :
 $(function () {
-    $("#submit_id").on("click", function () {
-        let userEmail, userPassword;
+    $("#btnSubmit").click(function () {
+        let user_email, user_password;
         //email
+        user_email = $("#user_email").val();
+        user_email = jQuery.trim(user_email);
+        //alert(user_email);
+
         //password
-    })
+        user_password = $("#user_password").val();
+        user_password = jQuery.trim(user_password);
+        //alert(user_password);
+
+        /* email */
+        if (user_email == "") {
+            $('#validation_email').html("Email alanı boş geçemezsiniz");
+        } else if (validateEmail(user_email) == false) {
+            $('#validation_email').html("Email'i uygun formatta yazmadınız. örnek: deneme@xyz.com")
+        }
+
+        /* validation email */
+        let validateEmail = (user_email) => {
+            const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
+            return regex.test(user_email);
+        }
+
+        /* password */
+        if (user_password == "") {
+            $('#validation_password').html("Şifre Alanı boş geçilemez");
+        }
+
+
+
+
+    });
 })
 
 
